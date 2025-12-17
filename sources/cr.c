bf4279c7abf5e7161a522e02e710f426bd761483
evgeniygazetdinov <zplacesound@gmail.com>
2025-12-17 11:13:15 +0300

    in process punkt chars fix

--- sources/cr.c
+++ sources/cr.c
@@ -2,6 +2,7 @@
 #include "stdio.h"
 #include "string.h"
 #include "helper.h"
+#include "ctype.h"
 
 char LOWERCASE_ALPHABET[] = "abcdefghijklmnopqrstuvwxyz";
 char UPPERCASE_ALPHABET[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
@@ -16,28 +17,31 @@ const int ALPHABET_SIZE = 29;
 
 int is_punkt_char(char * char_for_compare){
   if (*char_for_compare == ' ' || *char_for_compare == ',' 
-    || *char_for_compare == '\n'|| *char_for_compare){
+    || *char_for_compare == '\n'){
+
         return 1;
     }
     return 0;
 }
 
-void handle_punkt_chars(char* array_for, int * position){
-    if (array_for[*position] == ' '){
+void handle_punkt_chars(char *array_for, int * position, char * crypted_array){
+    if (isspace(array_for[*position]) || array_for[*position] == ' ' || array_for[*position] == NULL){
+      crypted_array[*position] = ' ';
       array_for[*position] = ' ';
     }
-    else if (array_for[*position] == '\n'){
-      array_for[*position] = '\n';
-    }
+    // else if (array_for[*position] == '\n'){
+    //   array_for[*position] = '\n';
+    // }
     else if (array_for[*position] == ','){
-      array_for[*position] = ',';
+      crypted_array[*position] = ',';
+      printf("################################################# here\n");
     }
-    else if (array_for[*position] == ';'){
-      array_for[*position] = ';';
-    }
-    else if (array_for[*position] == '\t'){
-      array_for[*position] = '\t';
-    }
+    // else if (array_for[*position] == ';'){
+    //   array_for[*position] = ';';
+    // }
+    // else if (array_for[*position] == '\t'){
+    //   array_for[*position] = '\t';
+    // }
 }
 
 
@@ -66,12 +70,10 @@ int find_position(char for_find, char **current_case)
 int get_new_position(int *position, char **current_case, char **array_for_choose)
 {
   int new_position;
-  if (*position == 666){
-    new_position = 666;
-  }
-  else{
-    new_position = (*position + SHIFT) % ALPHABET_SIZE;
-  }
+
+
+  new_position = (*position + SHIFT) % ALPHABET_SIZE;
+
   if (new_position < 0)
   {
     new_position += ALPHABET_SIZE;
@@ -134,13 +136,10 @@ void crypt(char *array_data, int *file_size)
   for (int i = 0; i < counter; i++)
   {
     char *current_case;
-    if(is_punkt_char(array_data[i])){
-      handle_punkt_chars(&array_data, &i);
-
+    if(is_punkt_char(&array_data[i])){
+      handle_punkt_chars(array_data, &counter, crypted_array);
     }
-    else{
-
-    
+    else{  
       int position = find_position(array_data[i], &current_case);
       char *array_with_chars;
       int new_position = get_new_position(&position, &current_case, &array_with_chars);
@@ -154,27 +153,27 @@ void crypt(char *array_data, int *file_size)
     array_data[i] = crypted_array[i];
   }
 }
-void decrypt(char *array_data, int *file_size)
-{
-  int counter = *file_size;
-  char decript_array[counter];
-  for (int i = 0; i < counter; i++)
-  {
-    char *current_case;
-    int position = find_position(array_data[i], &current_case);
-    char *array_with_chars;
-    if (is_punkt_char(decript_array[i])){
-      handle_punkt_chars(&decript_array, &i);
-    }
-    else{
-      int new_position = decript_position(&position, &current_case, &array_with_chars);
-      decript_array[i] = array_with_chars[new_position];
-    }
+// void decrypt(char *array_data, int *file_size)
+// {
+//   int counter = *file_size;
+//   char decript_array[counter];
+//   for (int i = 0; i < counter; i++)
+//   {
+//     char *current_case;
+//     int position = find_position(array_data[i], &current_case);
+//     char *array_with_chars;
+//     if (is_punkt_char(&decript_array[i])){
+//       handle_punkt_chars(array_data, &i, decript_array);
+//     }
+//     else{
+//       int new_position = decript_position(&position, &current_case, &array_with_chars);
+//       decript_array[i] = array_with_chars[new_position];
+//     }
 
-  }
-  for (int i = 0; i < counter; i++)
-  {
+//   }
+//   for (int i = 0; i < counter; i++)
+//   {
 
-    array_data[i] = decript_array[i];
-  }
-}
+//     array_data[i] = decript_array[i];
+//   }
+// }
